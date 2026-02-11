7e374ec2fbf20deebcb8c7e4701ac0cc2fd42643
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2026-02-11 01:25:51 +0100

    deployedSepoliaArc

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,33 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+require("dotenv").config({ path: "../.env" });
+
+
+const { SEPOLIA_URL, PRIVATE_KEY, ETHERSCAN_API_KEY, ARC_RPC_URL } = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+
+
+
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      // chainId: 11155111,
+    },
+    arcTestnet: {
+      url: ARC_RPC_URL,          // https://rpc.testnet.arc.network
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      // chainId: 5042002,
+    },
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`,
+  },
+
+  
+};
+
+export default config;
